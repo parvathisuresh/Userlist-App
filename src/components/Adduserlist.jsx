5408d42532d275ddfe64a103c0d7437c313b0d0d
parvathisuresh <parvathisuresh0220@gmail.com>
2024-07-10 10:10:03 +0530

    Userlist App

---
+++ src/components/Adduserlist.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Adduserlist = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Name</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Username</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Email</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Phone num</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Website</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <button className="btn btn-info">Add</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Adduserlist
---
+++ src/components/Searchuserlist.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Searchuserlist = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Searchuserlist
