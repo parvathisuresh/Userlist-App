baa7c22c81b85dbb33bbd54e6378ba406953b2b6
parvathisuresh <parvathisuresh0220@gmail.com>
2024-07-11 20:12:17 +0530

    userlist app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Adduserlist from './components/Adduserlist';
+import Searchuserlist from './components/Searchuserlist';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Viewall from './components/Viewall';
 
 function App() {
   return (
     <div>
-    <Adduserlist/>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/"element={<Adduserlist/>}/>
+    <Route path="/search"element={<Searchuserlist/>}/>
+    <Route path="/view"element={<Viewall/>}/>
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
--- src/components/Searchuserlist.jsx
+++ src/components/Searchuserlist.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Searchuserlist = () => {
+const [data,changeData]=useState(
+  [
+    {}
+  ]
+)  
   return (
     <div>
-      
+     <div className="container">
+      <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+          <div className="row g-3">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+              <label htmlFor="" className="form-label">Name</label>
+              <input type="text" className="form-control" />
+            </div>
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+              <button className="btn btn-success">Search</button>
+            </div>
+          </div>
+        </div>
+      </div>
+     </div> 
     </div>
   )
 }
